95d9b9bcbaeb06cc3957c8732d23595a6894a274
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-11-07 16:17:27 +0100

    Steg 4

--- Näsan-2/Program.cs
+++ Näsan-2/Program.cs
@@ -10,4 +10,7 @@ foreach (string toy in toys)
 // Punkt 3
 List<string> classmates = ["Linnea", "Amira", "Tea", "Aleks", "Ana"];
 
+// Punkt 4
+int[] numbers = new int[5] {1, 4, 5, 7, 9};
+
 Console.ReadLine();
