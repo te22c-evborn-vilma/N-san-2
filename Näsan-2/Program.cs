737b66c6cc402f91e247409dabdac57621980ab8
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-11-07 16:22:36 +0100

    Steg 5

--- Näsan-2/Program.cs
+++ Näsan-2/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Punkt 1
 string[] toys = ["Car", "Plane", "Doll", "Teddybear", "Lego"];
 
-// Punkt 2
-foreach (string toy in toys)
-{
-    Console.WriteLine(toy);
-}
-
 // Punkt 3
 List<string> classmates = ["Linnea", "Amira", "Tea", "Aleks", "Ana"];
 
 // Punkt 4
 int[] numbers = new int[5] {1, 4, 5, 7, 9};
+
+// Punkt 2 och 5
+for (int i = 0; i < toys.Length; i++)
+{
+    Console.WriteLine($"{classmates[i]} gives the {toys[i]} the rate {numbers[i]}");   
+}
 
 Console.ReadLine();
